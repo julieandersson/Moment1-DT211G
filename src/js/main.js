47b076a1783550ad9a210d504940e27c9e6fd44e
julieandersson <juan2301@student.miun.se>
2024-01-25 15:06:21 +0100

    skapar en klocka med aktuell tid med javascript på samtliga htmlsidor samt main och sectionelement

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,20 @@ function toggleMenu() {
         navMenuEl.style.display = "none";
     }
 }
+
+// Skapar en aktuell klocka som ska visas på hemsidan
+
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+  
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').innerHTML = timeString;
+  }
+  
+  // Updatera klocka varje sekund
+  setInterval(updateClock, 1000);
+  
+  updateClock();
